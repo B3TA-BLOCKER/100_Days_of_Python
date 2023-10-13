bc778fafa6880b8cfc91d71a9f46e79518a4e4b3
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2023-10-13 21:50:23 +0500

    Update Number_switching.py

---
+++ Day_1/Number_switching.py
@@ -0,0 +1,11 @@
+num_1 = input("Enter first Number : ")
+num_2 = input("Enter Second Number : ")
+
+temp = num_1
+num_1=num_2
+num_2=temp
+
+print("\n")
+print("After Swaping : ")
+print(f"First number = {num_1}")
+print(f"Second number = {num_2}")
