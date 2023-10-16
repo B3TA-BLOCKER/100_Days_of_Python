e1631a24c6f842a7ac6299632c948950fd87fcf8
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2023-10-16 22:29:18 +0500

    Update Banker_Roulette.py

--- Day_4/Banker_Roulette.py
+++ Day_4/Banker_Roulette.py
@@ -7,4 +7,5 @@ names = names_string.split(", ")
 num_item = len(names)
 index= random.randint(0,num_item-1)
 
+# Printing the final outcome using "fstring"
 print(f"{names[index]} is going to buy the meal today!")
