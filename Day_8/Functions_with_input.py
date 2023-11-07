f138f4fedc4748112f0711c17b5aef4b1626b713
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2023-11-07 14:10:13 +0500

    Add files via upload

---
+++ Day_8/Functions_with_input.py
@@ -0,0 +1,10 @@
+# Function that allows input
+
+def greet_with_name(name):
+    print(f"Hello {name} ")
+    print("How do you do? ")
+    print("Isn't the weather nice today? ")
+
+greet_with_name("Ben")
+print("\n")
+greet_with_name("Malik")
